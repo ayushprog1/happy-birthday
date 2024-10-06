1153de5a52fb7ed77f8f65a8e2c83ea5cf275ae0
ayush <AY.STUDY.ACC@gmail.com>
2024-10-06 09:30:02 -0700

    initialize file

---
+++ happybirthday.py
@@ -0,0 +1,8 @@
+import random
+import time
+for i in range(10000):
+   l=random.randint(1,1000)
+   j=random.randint(1,1000)
+   k=random.randint(1,1000)
+   print(" "*l,"*"," "*j,"*"," "*k,"Happy Birthday To You")
+   time.sleep(1)
